4039295763d186a867d9bb7b2297ad63c99823c0
veethree <69903178+veethree@users.noreply.github.com>
2021-08-29 01:05:21 +0000

    Add files via upload
    Cleaned up the word list a bit

--- main.py
+++ main.py
@@ -5,19 +5,41 @@ app = Flask(__name__)
 
 @app.route("/", methods=["POST", "GET"])
 def index():
-    if "word" in request.form:
-        word = request.form.get("word").strip()
-        with open("static/eminem_word_list.json", "r") as f:
-            word_list = json.load(f)
+    with open("static/eminem_word_list.json", "r") as f:
+        word_list = json.load(f)
 
+    sorted_list = sorted(word_list.items(), key=lambda x: x[1], reverse=True)
+    top_list = {}
+    for key,val in enumerate(sorted_list):
+        top_list[key] = val
+        if len(top_list) > 99:
+            break
+
+    if "word" in request.form:
+        word = request.form.get("word").strip().lower()
+        
         if word in word_list:
             word_count = word_list[word]
         else:
             word_count = 0
 
-        return render_template("index.html", word=word, word_count=word_count)
+        return render_template("index.html", word=word, word_count=word_count, top_list=top_list)
     else:
-        return render_template("index.html")
+        return render_template("index.html", top_list=top_list)
+
+@app.route("/top")
+def top():
+    with open("static/eminem_word_list.json", "r") as f:
+        word_list = json.load(f)
+
+    sorted_list = sorted(word_list.items(), key=lambda x: x[1], reverse=True)
+    top_list = {}
+    for key,val in enumerate(sorted_list):
+        top_list[key] = val
+        if len(top_list) > 99:
+            break
+
+    return render_template("top100.html", top_list=top_list)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
